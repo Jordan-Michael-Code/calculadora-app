237ae35de8a12d34827734396054d66e951d4bcf
unknown <[200~>
2024-03-04 01:55:59 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Calculadora';
+  numberA: number = 0;
+  numberB: number = 0;
+  result: number = 0;
+  sumar(): void {
+    this.result = this.numberA + this.numberB;
+  }
+}
